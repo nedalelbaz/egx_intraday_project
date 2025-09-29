5453913676eedb43e345ac0dbf521509f2a6c8ef
nedalelbaz <nedalelbaz@icloud.com>
2025-09-29 13:15:52 -0700

    Create telegram_notifier.py

---
+++ egx/utils/telegram_notifier.py
@@ -0,0 +1,13 @@
+import os
+import requests
+
+
+def send_message(text: str):
+    token = os.environ.get("TELEGRAM_BOT_TOKEN")
+    chat_id = os.environ.get("TELEGRAM_CHAT_ID")
+    if not token or not chat_id:
+        raise RuntimeError("Missing Telegram bot token or chat ID.")
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    data = {"chat_id": chat_id, "text": text}
+    response = requests.post(url, data=data)
+    response.raise_for_status()
